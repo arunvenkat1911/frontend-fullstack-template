33edcd6891c8bc5bc384b824f8f8c14d0e76f8c0
Arun <arunkumar.venkatesan@arcadix.com>
2023-01-05 18:05:04 +0530

    update

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ import { Router } from '@reach/router';
 import Posts from './components/posts';
 import Post from './components/post';
 
-function App() {  return (    <Router>      <Posts path="/" />      <Post path="/posts/:id" />    </Router>  );
+function App() {  
+    return (    
+    <Router>      
+        <Posts path="/" />      
+        <Post path="/posts/:id" />    
+    </Router>  );
 }
 
 export default App;
